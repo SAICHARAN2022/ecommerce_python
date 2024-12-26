8a19a156f2933d36a591ee0c333302d77f97250f
saicharanvss <saicharan.kyatham@vishleshan.net>
2024-12-26 14:47:52 +0530

    changes

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -160,9 +160,9 @@ ALLOWED_HOSTS = ['ecommerce.onrender.com']
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 
-MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_ROOT = BASE_DIR / 'media'
 
